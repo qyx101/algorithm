0c37314a945dcfdbe577d32cd416875cee1e2414
qyx101 <xmqyx1129@gmail.com>
2024-04-24 20:19:47 +0800

    // 无序数组中找第K大的数

--- 随机选择/findKthLargest.java
+++ 随机选择/findKthLargest.java
@@ -1,56 +1,57 @@
+// 无序数组中找第K大的数
+// https://leetcode.cn/problems/kth-largest-element-in-an-array/description/
+
 class Solution {
-    public static int findKthLargest(int[] arr, int k) {
-        // 1 2 3 4 5
-        // 假设 k=2, 找第二大的数 -> 下标: 3
-        // 假设 k=1, 找第一大的数 -> 下标: 4
-        return randomizedSelect(arr,arr.length - k);
+
+    // 思路转换:
+    // 无序数组中找第K大的数 ---> 从小到大排序, arr[数组长度 - k] = 第K大的数
+
+    // 1 2 3 4 5
+    // 假设 k=2, 找第二大的数 -> 下标: 3
+    // 假设 k=1, 找第一大的数 -> 下标: 4
+
+    public int findKthLargest(int[] arr, int k) {
+        return randomizedSelect(arr,arr.length-k);
     }
 
-    private static int randomizedSelect(int[] arr,int i) {
-        int ans = 0;
+    // first、last指针一定指向 == x 的区域
+    // first - 1 < x 
+    // last + 1 > x
+    public static int randomizedSelect(int[] arr,int index) {
         for (int l = 0,r = arr.length-1;l <= r;) {
-
-            // > x || = x || > x
-            int x = arr[l + (int) (Math.random() * (r - l + 1))];
-            partition(arr,l,r,x);
-
-            if (i < first) {
-                r = first-1;
-            }else if (i > last){
+            int x = arr[l + (int)(Math.random() * (r - l + 1))];
+            partition(arr,x,l,r);
+            if (index > last) {
                 l = last + 1;
+            } else if (index < first) {
+                r = first -1;
             }else {
-                ans = arr[i];
-                return ans;
+                return arr[index];
             }
         }
         return -1;
     }
+    private static int first,last;
 
-    public static int first,last;
-
-    private static void partition(int[] arr,int l,int r,int x) {
+    private static void partition(int[] arr,int x,int l,int r) {
         first = l;
         last = r;
         int i = l;
         while (i <= last) {
-            if (arr[i] < x) {
-                swap(arr,first++,i++);
-            }else if(arr[i] > x) {
+            if (arr[i] > x) {
                 swap(arr,i,last--);
+            }else if (arr[i] < x) {
+                swap(arr,i++,first++);
             }else {
                 i++;
             }
         }
     }
 
-    private static void swap(int[]arr,int i,int j) {
+    private static void swap(int[] arr,int i,int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
     }
 
-    public static void main(String[] args) {
-        int[] arr = {3,2,1,5,6,4};
-        findKthLargest(arr,2);
-    }
 }
